9f045d4ee6da09219c5d0400be9a9e3193677be7
Saba Amiri <amirisaba043@gmail.com>
2023-07-31 17:17:01 +0330

    edit

---
+++ 11-three-part/function11.js
@@ -0,0 +1,27 @@
+
+function firstPart(number) {
+    let sum = "";
+    for (let i = 1; (number * i) <= 100; i++) {
+        sum = sum + (number * i) + " ";
+
+        let plus="";
+        let num= (number*i).length;
+        for(let w=0 ; w<=(num-1); w++){
+          plus= plus + Number((number*i[w]))
+        }
+
+        return plus
+    }
+
+    
+}
+
+    // function secondPart(firstPart) {
+    //     let num = 0;
+    //     let plus = sum.length
+    //     for (let w = 0; w <= plus; w++) {
+    //         num = num + sum[w]
+    //     }
+
+    //     return num
+    // }
