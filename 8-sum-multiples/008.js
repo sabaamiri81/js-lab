8ea8ecb37e03f4be3812df239fc0aaf3a5ffc72f
Saba Amiri <amirisaba043@gmail.com>
2023-07-31 17:19:26 +0330

    edit

--- 8-sum-multiples/008.js
+++ 8-sum-multiples/008.js
@@ -1,6 +1,6 @@
 let num1 = Number(prompt("enter first number"));
 let num2 = Number(prompt("enter second number"));
-let res = saba(num1, num2);
+let res = sumMul(num1, num2);
 document.write(res);
 
 
--- 8-sum-multiples/function8.js
+++ 8-sum-multiples/function8.js
@@ -1,16 +1,34 @@
 let sum = 0;
-function saba(m, n) {
-    if (n > 0) {
-
+function sumMul(m, n) {
+    if (m > n)
+        return "INVALID"
         for (let i = 1; (m * i) < n; i++) {
 
             sum += (m * i)
         }
 
-    }
-
     return sum
 
 }
+
+// another way
+
+// let sum = 0;
+// function sumMul(m, n) {
+
+// if (n < m) {
+//     return "INVALID"
+// }
+
+//     else{
+
+//         for (let i = 0; i < n; i += m) {
+//             sum += i
+//         }
+//     }
+
+//     return sum
+// }
+
 
 
