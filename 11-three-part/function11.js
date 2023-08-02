ee8833ae737f0df8fe9fadeee772b52f0f6aa924
Saba Amiri <amirisaba043@gmail.com>
2023-08-02 13:27:50 +0330

    delete debbuger

--- 11-three-part/function11.js
+++ 11-three-part/function11.js
@@ -1,13 +1,16 @@
 
 function firstPart(number) {
     let sum = "";
+    debugger;
     for (let i = 1; (number * i) <= 100; i++) {
+        debugger;
         sum = sum + (number * i) + " ";
 
         let plus="";
         let num= (number*i).length;
         for(let w=0 ; w<=(num-1); w++){
           plus= plus + Number((number*i[w]))
+
         }
 
         return plus
