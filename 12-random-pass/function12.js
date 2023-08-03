b78c21d879b62c52b4271490740903a942ec62d9
Saba Amiri <amirisaba043@gmail.com>
2023-08-03 09:50:04 +0330

    three different part

---
+++ 12-random-pass/function12.js
@@ -0,0 +1,26 @@
+function number() {
+    let pass1 = "";
+    for (i = 0; i < 8; i++) {
+        pass1 += Math.floor(Math.random() * 10)
+    }
+
+    return pass1
+}
+
+function capital() {
+    let pass2 = "";
+
+    for (i = 0; i < 8; i++) {
+        pass2 += (String.fromCharCode(Math.floor(Math.random() * 26 + 65)))
+    }
+    return pass2
+}
+
+function small() {
+    let pass3 = "";
+
+    for (i = 0; i < 8; i++) {
+        pass3 += (String.fromCharCode(Math.floor(Math.random() * 26 + 97)))
+    }
+    return pass3
+}
