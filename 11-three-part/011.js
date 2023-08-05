9c95a8403d5055cb2597ec1b1321cb76fe9fc8e1
Saba Amiri <amirisaba043@gmail.com>
2023-08-05 13:05:05 +0330

    complete

---
+++ 11-three-part/011.js
@@ -0,0 +1,5 @@
+let number= Number(prompt("enter a number \n it should be under 100"));
+
+let answer= sumDigit(firstPart(number));
+
+document.write(answer)
--- 11-three-part/function11.js
+++ 11-three-part/function11.js
@@ -1,30 +1,19 @@
 
 function firstPart(number) {
     let sum = "";
-    debugger;
     for (let i = 1; (number * i) <= 100; i++) {
-        debugger;
+
         sum = sum + (number * i) + " ";
-
-        let plus="";
-        let num= (number*i).length;
-        for(let w=0 ; w<=(num-1); w++){
-          plus= plus + Number((number*i[w]))
-
-        }
-
-        return plus
     }
-
-    
+    return sum
 }
 
-    // function secondPart(firstPart) {
-    //     let num = 0;
-    //     let plus = sum.length
-    //     for (let w = 0; w <= plus; w++) {
-    //         num = num + sum[w]
-    //     }
-
-    //     return num
-    // }
+function sumDigit(input) {
+    let input1 = String(input);
+    let count = input1.length;
+    let num = 0
+    for (let w = 0; w < count; w++){
+        num+= Number(input1[w]);
+    }
+    return num
+}
