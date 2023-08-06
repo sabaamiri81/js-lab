0d17acd2be5d38801a53d7066868bfb631a61969
Saba Amiri <amirisaba043@gmail.com>
2023-08-06 10:04:23 +0330

    stringy practice

--- my.first,practice/function.js
+++ my.first,practice/function.js
@@ -1,9 +1,20 @@
-function findMultiples(integer, limit) {
-  let sum = "";
-  for (let i = 1; (integer * i) <= limit; i++) {
+// function findMultiples(integer, limit) {
+//   let sum = "";
+//   for (let i = 1; (integer * i) <= limit; i++) {
 
-    sum += "[" + integer * i + "]" + " ";
+//     sum += "[" + integer * i + "]" + " ";
+//   }
+
+//   console.log(sum)
+// };
+
+function stringy(size) {
+  let size1 = String(size);
+  let count = size1.length;
+  let sum = "";
+  for (let i = 0; i = count; i++) {
+    sum += 1 | 0;
   }
 
-  console.log(sum)
-};
+  return sum
+}
