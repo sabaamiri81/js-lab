e2f75f07b5bc1fc9dde72e37d9a17c6fad521ff8
Saba Amiri <amirisaba043@gmail.com>
2023-08-08 14:03:04 +0330

    i did all task successfully

---
+++ 17-colorful-box/function17.js
@@ -0,0 +1,22 @@
+// DOM NODES
+
+let thumbs = document.querySelectorAll(".box");
+console.log(thumbs);
+
+// FUNCTIONS
+
+function random(a, b) {
+    return Math.floor(Math.random() * (b - a) + a)
+}
+
+function colorfulBox() {
+     this.style.backgroundColor = "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")"
+}
+
+// events
+
+for (let index = 0; index < thumbs.length; index++) {
+    thumbs[index].onclick =  colorfulBox;
+
+}
+
