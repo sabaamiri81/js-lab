4ae19ac6484694004d6e40eda1101fca58d10957
Saba Amiri <amirisaba043@gmail.com>
2023-08-12 11:58:59 +0330

    create abdollah function

--- 18-abdolah-diet/function18.js
+++ 18-abdolah-diet/function18.js
@@ -1,6 +1,29 @@
+let table = ["G", "Y", "G", "R", "R"]
+
+function diet(array) {
+    let green = []
+    let yellow = []
+    let red = []
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+
+        if (array[i] === "G") {
+            green.push(array[i])
+        }
+        if (array[i] === "R") {
+            red.push(array[i])
+        }
+        if (array[i] === "Y") {
+            yellow.push(array[i])
+        }
+
+    }
+
+    if (red.length >= 3 || red.length === 5 || yellow.length === 5 || green.length === 0 || green.length === 1) {
+        return "nakhor"
+    } else {
+        return "nosh"
+    }
 
 
-function diet(l1, l2,l3,l4,l5) {
-    
-    
 }
