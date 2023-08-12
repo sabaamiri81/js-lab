4c0cf8cb801c4792607446e55ec494672ce68140
Saba Amiri <amirisaba043@gmail.com>
2023-08-12 12:19:57 +0330

    edit

--- 18-abdolah-diet/function18.js
+++ 18-abdolah-diet/function18.js
@@ -1,12 +1,10 @@
 let table = ["G", "Y", "G", "R", "R"]
-
 function diet(array) {
     let green = []
     let yellow = []
     let red = []
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
-
         if (array[i] === "G") {
             green.push(array[i])
         }
@@ -16,14 +14,10 @@ function diet(array) {
         if (array[i] === "Y") {
             yellow.push(array[i])
         }
-
     }
-
     if (red.length >= 3 || red.length === 5 || yellow.length === 5 || green.length === 0 || green.length === 1) {
         return "nakhor"
     } else {
         return "nosh"
     }
-
-
 }
