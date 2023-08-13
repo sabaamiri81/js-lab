c7e6930635d8bd1afffda167c68dc892db4cb4b8
Saba Amiri <amirisaba043@gmail.com>
2023-08-13 12:52:18 +0330

    add switch case instead of if(condition)

--- 18-abdolah-diet/function18.js
+++ 18-abdolah-diet/function18.js
@@ -1,20 +1,40 @@
-let table = ["G", "Y", "G", "R", "R"]
+let lable = ["G", "Y", "R", "R", "R"]
+let lable2= "GYRRR"
+
 function diet(array) {
     let green = []
     let yellow = []
     let red = []
     for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        if (array[i] === "G") {
-            green.push(array[i])
+
+        // if (array[i] === "G") {
+        //     green.push(array[i])
+        // }
+        // if (array[i] === "R") {
+        //     red.push(array[i])
+        // }
+        // if (array[i] === "Y") {
+        //     yellow.push(array[i])
+        // }
+
+        switch (array[i]) {
+            case "R":
+                red.push(array[i]);
+                break;
+
+            case "Y":
+                yellow.push(array[i]);
+                break;
+
+            case "G":
+                green.push(array[i])
+                break;
         }
-        if (array[i] === "R") {
-            red.push(array[i])
-        }
-        if (array[i] === "Y") {
-            yellow.push(array[i])
-        }
+
+
+
     }
+
     if (red.length >= 3 || red.length === 5 || yellow.length === 5 || green.length === 0 || green.length === 1) {
         return "nakhor"
     } else {
