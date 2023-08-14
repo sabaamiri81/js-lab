da1fc58de469b6651adefbadc6ed3aad679270d1
Saba Amiri <amirisaba043@gmail.com>
2023-08-14 09:40:43 +0330

    edit btn position

--- 23-mouse-movment/function23.js
+++ 23-mouse-movment/function23.js
@@ -5,40 +5,38 @@ let btn = document.querySelector("button")
 
 let flag = true
 function toggle() {
-    if(flag=== true){
-        body.style.backgroundColor =  "rgb(133, 211, 239)";
+    if (flag === true) {
+        body.style.backgroundColor = "rgb(133, 211, 239)";
 
         btn.textContent = "last night";
 
-        circle.style.backgroundImage= "linear-gradient(yellow, orange,rgb(246, 217, 138), rgb(255, 172, 47))" ;
-        circle.style.boxShadow= "20px 20px 70px rgb(228, 211, 25)";
+        circle.style.backgroundImage = "linear-gradient(yellow, orange,rgb(246, 217, 138), rgb(255, 172, 47))";
+        circle.style.boxShadow = "20px 20px 70px rgb(228, 211, 25)";
 
         flag = false
-    }else{
+    } else {
         body.style.backgroundColor = "rgb(15, 35, 102)";
-        
+
         btn.textContent = "next day";
 
-        circle.style.backgroundImage= "linear-gradient(white,rgb(183, 178, 178),rgb(214, 207, 207),rgb(234, 229, 213),rgb(202, 185, 185),rgb(201, 189, 189))" ;
-        circle.style.boxShadow= "20px 20px 70px rgb(255, 255, 255)";
+        circle.style.backgroundImage = "linear-gradient(white,rgb(183, 178, 178),rgb(214, 207, 207),rgb(234, 229, 213),rgb(202, 185, 185),rgb(201, 189, 189))";
+        circle.style.boxShadow = "20px 20px 70px rgb(255, 255, 255)";
 
         flag = true;
     }
-
-    
 }
 
 function trackCircle(event) {
     console.log(event)
-    let x = event.screenX /2
-    let y = event.screenY /2
+    let x = event.screenX / 2
+    let y = event.screenY / 2
 
-    let a = event.movementX /2
-    let b = event.movementY /2
+    let a = event.movementX / 2
+    let b = event.movementY / 2
 
 
     // template string
-   
+
     circle.style.transform = `translate(${a}px,${b}px)`;
     circle.style.transform = `translate(${x}px,${y}px)`;
 }
