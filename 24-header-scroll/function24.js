5f0b05e8e6293688d22e820c5bf023a8664188c1
Saba Amiri <amirisaba043@gmail.com>
2023-08-14 10:35:41 +0330

    create header scroll

---
+++ 24-header-scroll/function24.js
@@ -0,0 +1,14 @@
+let header = document.querySelector("header")
+let oldScroll = 0
+
+function scrollHeader() {
+    if (window.scrollY > oldScroll) {
+        header.classList.remove("change");
+    } else {
+        header.classList.add("change");
+        header.style.transition= "3s";
+    }
+    oldScroll = window.scrollY
+}
+
+window.addEventListener("scroll",scrollHeader );
