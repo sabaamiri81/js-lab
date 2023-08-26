e6205d953af8cb0e7ab8d64e3bc709bb1b9b213a
Saba Amiri <amirisaba043@gmail.com>
2023-08-26 17:31:51 +0330

    edit

--- 25-digi-object/function25.js
+++ 25-digi-object/function25.js
@@ -1,6 +1,6 @@
 const CAMERA = {
     title: "دوربین دیجیتال کانن مدل EOS 4000D به همراه لنز 18-55 میلی متر DC III",
-    price: "۱۴۷۹۹۰۰۰" ,
+    price: "۱۴۷۹۹۰۰۰۰" ,
     imageUrl: "./image/112439699.webp",
     rate : "۴ ",
 }
@@ -10,7 +10,21 @@ let title = document.querySelector("h1");
 let price = document.querySelector("h2");
 let rate = document.querySelector("p");
 
+function exchange(){
+    let price = CAMERA.price/ 10;
+    return price.toLocaleString();
+}
+
 img.setAttribute("src" , CAMERA.imageUrl);
 title.textContent= CAMERA.title;
-price.textContent = "قیمت : "  + CAMERA.price + " "+"تومان" ;
+price.textContent = "قیمت : "  + exchange() + " "+"تومان" ;
 rate.textContent= "امتیاز : " + CAMERA.rate;
+
+
+sum = ""
+function exchange(input) {
+    for(let i = input.length-1 ; i>= 0 ; i-3){
+       sum+= input[i] + input[i+1] + input[i+2] +","
+    }
+    return sum
+}
